1027b46daa4b18d54d5bf7b455ef2c8559ac8ae6
g4n1d <jonatanciencias@gmail.com>
2023-12-26 16:13:57 -0500

    Add functionality to calculate total bill with tip percentage

---
+++ proyectos/calduladora-de-propinas/app.js
@@ -0,0 +1,33 @@
+/*
+Cornell Note:
+- Notes: 
+  - Bringing the button element from the DOM.
+  - Bringing the bill input element from the DOM.
+  - Bringing the tip input element from the DOM.
+  - Bringing the total element from the DOM.
+  - Add an event listener to the button.
+    - Create a function that will calculate the total.
+      - Get the value of the bill input.
+      - Get the value of the tip input.
+      - Calculate the total multiplying the bill value by porcentage of the tip.
+      - Print the total value in the total element (with two decimals)
+  - 
+- Questions:
+- Summary:
+*/
+
+const btnEl  = document.getElementById('calculate');
+const billInput = document.getElementById('bill'); 
+const tipInput = document.getElementById('tip');
+const totalEl = document.getElementById('total');
+
+btnEl.addEventListener('click',  calculateTotal => {
+  // console.log('Calculating...');
+  const billValue = billInput.value;
+  const tipValue = tipInput.value;
+  const totalValue = billValue * (1 +  (tipValue / 100));
+  totalEl.textContent = totalValue.toFixed(2);
+})
+
+
+
