fe1896fc94759d752f698808a9d4587520c3ce8c
g4n1d <jonatanciencias@gmail.com>
2024-01-05 21:15:24 -0500

    Add createBoard function

--- proyectos/juego-snake/app.js
+++ proyectos/juego-snake/app.js
@@ -1,3 +1,5 @@
+const create = require("prompt-sync");
+
 // Traemos todos los elementos que necesitamos del html
 const board = document.getElementById('board');
 const scoreBoard = document.getElementById('scoreBoard');
@@ -32,6 +34,12 @@ let emptySquares; // array con las posiciones vacías del tablero para generar l
 let moveInterval; // intervalo de tiempo para mover la serpiente
 
 
+// Función para crear el tablero
+const createBoard = () => {
+
+};
+
+
 const setGame = () => {
   // serpiente tiene un largo inicial de 4
   snake = ['00', '01', '02', '03'];
@@ -41,17 +49,24 @@ const setGame = () => {
   // Estrucutra de datos para guardar la información del tablero
   // Arras de dos dimensiones
   boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+  // reseteamos el juego borrando el tablero
+  board.innerHTML = '';
+  // este array se va a llenar a medida que se crea el tablero
+  emptySquares = [];
+  // se genera el tablero con la función createBoard
+  createBoard();
   console.log(boardSquares);
-}
+};
 
 // Funcion para setear el juego (darle valores a las variables del juego)
 const startGame = () => {
   // Cuando el juego termina, hay que setear de nuevo el juego
   setGame();
-}
+};
 
 
 
 // cuando se haga click se llama a la función startGame
 startButton.addEventListener('click', startGame);
+
 
