d57c51c67c0d4a530ba13e29d03962deb3f95389
g4n1d <jonatanciencias@gmail.com>
2024-01-20 19:35:21 -0500

    add some guide notes

---
+++ proyectos/dados/app.js
@@ -0,0 +1,10 @@
+/*
+Cornell Note:
+- Notes: 
+  - bring the the roll-button
+  - add the event listener to the roll-button
+  - create the function that will be executed when the roll-button is clicked
+- Questions:
+  - How to create a function inside the addEventListener method?
+- Summary:
+*/ 
