e5523bde07bca513eb6d329456484ce0fa2f5950
g4n1d <jonatanciencias@gmail.com>
2024-01-31 07:41:29 -0500

    Refactor code to improve performance and readability

--- proyectos/dados/app.js
+++ proyectos/dados/app.js
@@ -4,6 +4,11 @@ Cornell Note:
   - bring the the roll-button
   - add the event listener to the roll-button
   - create the function that will be executed when the roll-button is clicked
+  - bring the dice element
+  - add th animation class to the dice element
+  - add a setTimeout function to remove the animation class after 1 second
+
+  - function to generate a random number between 1 and 6
 - Questions:
   - How to create a function inside the addEventListener method?
 - Summary:
