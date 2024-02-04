aefc9e2ae00f448b2ef6358a7dcfefb996b67a5d
g4n1d <jonatanciencias@gmail.com>
2024-02-03 20:57:07 -0500

    Refactor dice roll history functionality

--- proyectos/dados/app.js
+++ proyectos/dados/app.js
@@ -14,15 +14,14 @@ Cornell Note:
   - function (rollDice) to generate a random number between 1 and 6 and round it
     - create a const (rollResult) to generate a random number between 1 and 6 and round it
     - call the function (getDiceFace) to change the image of the dice element
-    - change the innerHTML of the dice element to the new image.
 
   - function (getDiceFace) to change the image of the dice element
     - create a switch with 6 statement to change the image of the dice element depending on the random number generated.
     
   - Change the innerHTML of the dice element to the new image.(inside the rollDice function)
 
-  - Create a variable (HistoryLIst) with an empty array for the history of the dice rolls
-  - push the new dice roll to the HistoryList array with the rollResult
+  - Create a variable (HistoryList) with an empty array for the history of the dice rolls
+  - push the new dice  from rollDice function to the HistoryList array with the rollResult
   - call the function (updateRollHistory) to update the history of the dice rolls
 
   - function (updateRollHistory) to update the history of the dice rolls
@@ -30,7 +29,7 @@ Cornell Note:
     - create a for loop to iterate over the HistoryList array
       - create a new li element (const listItem)
       - add the rollResult to the innerHTML in a new li element with backticks
-        - create a getDiceFace functio that takes the historyList[i] as an argument
+        - create a getDiceFace function that takes the historyList[i] as an argument
       - append the new li element to the roll-history element
       - appendChild the listItem to the roll-history element
       - comment the roll-history original elements in the html file
@@ -54,6 +53,8 @@ const diceEl = document.getElementById ("dice");
 
 const rollHistory = document.getElementById("roll-history");
 
+let historyList = [];
+
 
 rollButton.addEventListener("click", () => {
   diceEl.classList.add("roll-animation");
@@ -68,6 +69,8 @@ function rollDice() {
   const rollResult = Math.floor(Math.random() * 6) + 1;
   const diceFace = getDiceFace(rollResult);
   diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+  updateRollHistory();
 }
 
 
@@ -87,6 +90,17 @@ function getDiceFace(rollResult) {
       return "&#9861;";
     default:
       return " ";
+  }
+
+}
+
+function updateRollHistory() {
+  rollHistory.innerHTML = "";
+  for (let h = 0; h < historyList.length; h++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Tirada ${h + 1}: <span> ${getDiceFace(historyList[h])} </span>`;
+    rollHistory.appendChild(listItem);
+
   }
 
 }
