7753265ef84d5cc95b3641ef2f010b0b79f36442
g4n1d <jonatanciencias@gmail.com>
2024-02-09 14:05:59 -0500

    Add start, stop, reset buttons and timer text with event listener

---
+++ proyectos/pomodoro/app.js
@@ -0,0 +1,15 @@
+/*
+Cornell Note:
+- Notes: 
+  - bring start button with the id of start
+  - bring stop button with the id of stop
+  - bring reset button with the id of reset
+  - bring timer text with the id of timer
+
+  - add event listener to start button and call startTimer function
+
+  - create startTimer function
+  
+- Questions:
+- Summary:
+*/
