a52074b26f26750ea1315fccd2d054cd10fdb3c8
g4n1d <jonatanciencias@gmail.com>
2024-02-11 20:49:47 -0500

    Go on with the instructions in the comment.

--- proyectos/pomodoro/app.js
+++ proyectos/pomodoro/app.js
@@ -6,9 +6,27 @@ Cornell Note:
   - bring reset button with the id of reset
   - bring timer text with the id of timer
 
-  - add event listener to start button and call startTimer function
+  - create a variable to store the interval
+  - create a variable to store the timeLeft (1500 seconds)
+
+  - create a function (updateTimer) to convert seconds to minutes and seconds
+    - create a variable to store the minutes (math.floor(timeLeft/ 60)
+    - create a variable to store the seconds (timeLeft % 60)
+    - create a variable to store the formatedTime (minutes + ':' + seconds)
+
+    - modify the timerEl text to display the formatedTime
 
   - create startTimer function
+    - set interval with setInterval function 
+    - decrement timeLeft by 1
+    - call updateTimer every second
+
+  - create stopTimer function
+  - create resetTimer function
+
+  - add event listener to start button and call startTimer function
+  - add event listener to stop button and call stopTimer function
+  - add event listener to reset button and call resetTimer function
   
 - Questions:
 - Summary:
